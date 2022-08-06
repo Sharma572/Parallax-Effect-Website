4bc49ea7c55f27a00c5b217e95b4ae3cdea6ca17
Raunak Sharma <raunakk572@gmail.com>
2022-08-06 14:36:01 +0530

    photo

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+class myHeader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <header class="header">
+        <div class="logo_container">
+          <img
+            src="./images/main-logo.png"
+            alt="Raunak technical logo"
+            class="logo"
+          />
+        </div>
+        <nav class="navbar">
+          <ul class="navbar-lists">
+            <li>
+              <a id="home" class="navbar-link home-link" href="#main">Home</a>
+            </li>
+            <li>
+              <a id="about" class="navbar-link about-link" href="#footer"
+                >About</a
+              >
+            </li>
+            <li>
+              <a
+                id="porfolio"
+                class="navbar-link portfolio-link"
+                href="#portfolio-section"
+                >Portfolio</a
+              >
+            </li>
+            <li>
+              <a
+                id="contact"
+                class="navbar-link contact-link"
+                href="#contact-section"
+                >Contact</a
+              >
+            </li>
+          </ul>
+        </nav>
+  
+        <div class="mobile-navbar-btn">
+          <span class="mobile-nav-icon" name="menu-outline"
+            ><img class="util-logo" src="images/menu-logo.png" alt="menu"
+          /></span>
+          <span class="mobile-nav-icon" name="close-outline"
+            ><img class="util-logo" src="images/close-btn.png" alt="close"
+          /></span>
+        </div>
+      </header>
+        `
+    }
+}
+
+customElements.define('my-header', myHeader);
+
+
+
+
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 const homebtn = document.getElementById('home');
